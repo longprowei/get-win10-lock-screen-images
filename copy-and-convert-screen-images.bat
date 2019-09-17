d6add8e2a4a21aaf52adc4bc76c795ecb8950ffd
Chenglong Wei <longprowei@gmail.com>
2019-09-17 17:00:03 +1000

    init file

---
+++ copy-and-convert-screen-images.bat
@@ -0,0 +1,3 @@
+mkdir %userprofile%\Desktop\Screen-Images
+copy %userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets %userprofile%\Desktop\Screen-Images
+rename %userprofile%\Desktop\Screen-Images\* *.jpg
